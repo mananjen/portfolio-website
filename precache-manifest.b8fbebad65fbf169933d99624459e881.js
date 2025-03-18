98cfa75cc46a0fb1233c374a1a3d57e814baefe7
Manan Jain <mananjen@gmail.com>
2025-03-18 10:14:46 -0500

    Updates

---
+++ precache-manifest.b8fbebad65fbf169933d99624459e881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "becb9b401c165311541f9831d1de219c",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "3705e786118974610e36",
+    "url": "/portfolio-website/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ffc4f8a4ae902508669b",
+    "url": "/portfolio-website/static/js/2.680fc8fd.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/portfolio-website/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "3705e786118974610e36",
+    "url": "/portfolio-website/static/js/main.3ffd6e2e.chunk.js"
+  },
+  {
+    "revision": "9cfefcdf39a4dba28487",
+    "url": "/portfolio-website/static/js/runtime~main.f259c5d0.js"
+  }
+]);
